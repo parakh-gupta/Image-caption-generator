8fdd571ae19066df33f0ca5415fe6a2cd948484c
Parakh Gupta <parakhgupta.98@gmail.com>
2020-08-07 19:31:20 +0530

    Added files via upload

---
+++ preprocess.py
@@ -0,0 +1,62 @@
+from pickle import load
+
+# load doc into memory
+def load_doc(filename):
+    # open the file as read only
+    file = open(filename, 'r')
+    # read all text
+    text = file.read()
+    # close the file
+    file.close()
+    return text
+
+# load a pre-defined list of photo identifiers
+def load_set(filename):
+    doc = load_doc(filename)
+    dataset = list()
+    # process line by line
+    for line in doc.split('\n'):
+        # skip empty lines
+        if len(line) < 1:
+            continue
+        # get the image identifier
+        identifier = line.split('.')[0]
+        dataset.append(identifier)
+    return set(dataset)
+
+# load photo features
+def load_photo_features(filename, dataset):
+    # load all features
+    all_features = load(open(filename, 'rb'))
+    # filter features
+    features = {k: all_features[k] for k in dataset}
+    return features
+
+# covert a dictionary of clean descriptions to a list of descriptions
+def to_lines(descriptions):
+    all_desc = list()
+    for key in descriptions.keys():
+        [all_desc.append(d) for d in descriptions[key]]
+    return all_desc
+ 
+
+# load clean descriptions into memory
+def load_clean_descriptions(filename, dataset):
+    # load document
+    doc = load_doc(filename)
+    descriptions = dict()
+    for line in doc.split('\n'):
+        # split line by white space
+        tokens = line.split()
+        # split id from description
+        image_id, image_desc = tokens[0], tokens[1:]
+        # skip images not in the set
+        if image_id in dataset:
+            # create list
+            if image_id not in descriptions:
+                descriptions[image_id] = list()
+            # wrap description in tokens
+            desc = 'startseq ' + ' '.join(image_desc) + ' endseq'
+            # store
+            descriptions[image_id].append(desc)
+    return descriptions
